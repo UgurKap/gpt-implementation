3768637714a823e694a7b2c70010f6a4e4b5d4b2
Uğur Ali Kaplan <uguralikaplan@gmail.com>
2025-11-28 14:37:12 +0100

    Added lr warmup, cosine decay and gradient clipping to gpt2 training

--- Chapters/gpt_training_utils.py
+++ Chapters/gpt_training_utils.py
@@ -135,3 +135,75 @@ def generate(
             break
         idx = torch.cat((idx, idx_next), dim=1)
     return idx
+
+
+import math
+
+
+def train_model_with_bells_and_whistles(
+    model,
+    train_loader,
+    val_loader,
+    optimizer,
+    device,
+    n_epochs,
+    eval_freq,
+    eval_iter,
+    start_context,
+    tokenizer,
+    warmup_steps,
+    initial_lr=3e-5,
+    min_lr=1e-6,
+):
+    train_losses, val_losses, track_tokens_seen, track_lrs = [], [], [], []
+    tokens_seen, global_step = 0, -1
+
+    peak_lr = optimizer.param_groups[0]["lr"]
+    total_training_steps = len(train_loader) * n_epochs
+    lr_increment = (peak_lr - initial_lr) / total_training_steps
+
+    for epoch in range(n_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            global_step += 1
+
+            if global_step < warmup_steps:
+                lr = initial_lr + global_step * lr_increment  # LR Warmup
+            else:
+                progress = (global_step - warmup_steps) / (
+                    total_training_steps - warmup_steps
+                )
+                lr = min_lr + (peak_lr - min_lr) * 0.5 * (
+                    1 + math.cos(math.pi * progress)
+                )  # Cosine decay
+
+            for param_group in optimizer.param_groups:
+                param_group["lr"] = lr
+            track_lrs.append(lr)
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+
+            if global_step >= warmup_steps:
+                torch.nn.utils.clip_grad_norm_(
+                    model.parameters(), max_norm=1.0
+                )  # Gradient Clipping
+
+            optimizer.step()
+            tokens_seen += input_batch.numel()
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(
+                    f"Ep {epoch + 1} (Iter {global_step:06d}): ",
+                    f"Train loss {train_loss:.3f}, ",
+                    f"Val loss {val_loss:.3f}",
+                )
+
+        generate_and_print_sample(model, tokenizer, device, start_context)
+    return train_losses, val_losses, track_tokens_seen, track_lrs
