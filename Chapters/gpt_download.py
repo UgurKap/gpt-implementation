858f901eeb32935e4acaaf283fb5288132622f23
Uğur Ali Kaplan <uguralikaplan@gmail.com>
2025-12-06 01:07:43 +0100

    Ruff formatting

--- Chapters/gpt_download.py
+++ Chapters/gpt_download.py
@@ -24,9 +24,13 @@ def download_and_load_gpt2(model_size, models_dir):
     base_url = "https://openaipublic.blob.core.windows.net/gpt-2/models"
     backup_base_url = "https://f001.backblazeb2.com/file/LLMs-from-scratch/gpt2"
     filenames = [
-        "checkpoint", "encoder.json", "hparams.json",
-        "model.ckpt.data-00000-of-00001", "model.ckpt.index",
-        "model.ckpt.meta", "vocab.bpe"
+        "checkpoint",
+        "encoder.json",
+        "hparams.json",
+        "model.ckpt.data-00000-of-00001",
+        "model.ckpt.index",
+        "model.ckpt.meta",
+        "vocab.bpe",
     ]
 
     # Download files
@@ -63,7 +67,9 @@ def download_file(url, destination, backup_url=None):
 
         block_size = 1024  # 1 KB
         desc = os.path.basename(download_url)
-        with tqdm(total=file_size, unit="iB", unit_scale=True, desc=desc) as progress_bar:
+        with tqdm(
+            total=file_size, unit="iB", unit_scale=True, desc=desc
+        ) as progress_bar:
             with open(destination, "wb") as file:
                 for chunk in response.iter_content(chunk_size=block_size):
                     if chunk:
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import torch
 import torch.nn as nn
 from torch.amp import autocast
 
-torch.set_float32_matmul_precision('high')
+torch.set_float32_matmul_precision("high")
+
 
 def main():
     device = torch.device("cuda")
@@ -14,13 +15,45 @@ def main():
     learning_rate = 1e-3
 
     # Toy dataset
-    x_train = torch.FloatTensor([[3.3], [4.4], [5.5], [6.71], [6.93], [4.168], 
-                        [9.779], [6.182], [7.59], [2.167], [7.042], 
-                        [10.791], [5.313], [7.997], [3.1]]).to(device)
+    x_train = torch.FloatTensor(
+        [
+            [3.3],
+            [4.4],
+            [5.5],
+            [6.71],
+            [6.93],
+            [4.168],
+            [9.779],
+            [6.182],
+            [7.59],
+            [2.167],
+            [7.042],
+            [10.791],
+            [5.313],
+            [7.997],
+            [3.1],
+        ]
+    ).to(device)
 
-    y_train = torch.FloatTensor([[1.7], [2.76], [2.09], [3.19], [1.694], [1.573], 
-                        [3.366], [2.596], [2.53], [1.221], [2.827], 
-                        [3.465], [1.65], [2.904], [1.3]]).to(device)
+    y_train = torch.FloatTensor(
+        [
+            [1.7],
+            [2.76],
+            [2.09],
+            [3.19],
+            [1.694],
+            [1.573],
+            [3.366],
+            [2.596],
+            [2.53],
+            [1.221],
+            [2.827],
+            [3.465],
+            [1.65],
+            [2.904],
+            [1.3],
+        ]
+    ).to(device)
 
     # Linear regression model
     model = torch.compile(nn.Linear(input_size, output_size).to(device))
@@ -31,23 +64,23 @@ def main():
 
     # Train the model
     for epoch in range(num_epochs):
-
         with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
             # Forward pass
             outputs = model(x_train)
             loss = criterion(outputs, y_train)
-        
+
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
-        if (epoch+1) % 5 == 0:
-            print ('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+
+        if (epoch + 1) % 5 == 0:
+            print(
+                "Epoch [{}/{}], Loss: {:.4f}".format(epoch + 1, num_epochs, loss.item())
+            )
 
     predicted = model(x_train)
     print(f"MAE: {torch.abs(predicted - y_train).mean().item()}")
-
 
 
 if __name__ == "__main__":
